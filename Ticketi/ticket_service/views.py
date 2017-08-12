75380c6d990711438eded58aa89ca56585acf1d8
SadeghHayeri <hayerisadegh@gmail.com>
2017-08-12 18:51:10 +0430

    paginations added for 'tickets' view!

--- Ticketi/ticket_service/views.py
+++ Ticketi/ticket_service/views.py
@@ -3,10 +3,17 @@
 from rest_framework import generics
 from .serializer import TicketSerializer
 from .models import Ticket
+from rest_framework.pagination import PageNumberPagination
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 10
 
 class TicketView(generics.ListCreateAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    pagination_class = StandardResultsSetPagination
 
     def perform_create(self, serializer):
         serializer.save()
