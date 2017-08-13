6317d9b995eb567f1833a84ba9ff3f3dd1c55ac5
SadeghHayeri <hayerisadegh@gmail.com>
2017-08-13 22:37:54 +0430

    - better ticket api -

--- ticket_service/models.py
+++ ticket_service/models.py
@@ -3,12 +3,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User, Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import datetime
+import json
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     post = models.CharField(max_length = 100)
-    profile_picture_path = models.CharField(max_length = 500)
+    picture_path = models.CharField(max_length = 500)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
 class Ticket (models.Model):
@@ -31,7 +43,7 @@ class Ticket (models.Model):
     unknown_approvers = models.ManyToManyField(User, related_name='ticket_M2M_unknown_approvers', blank=True)
     known_denials = models.ManyToManyField(User, related_name='ticket_M2M_known_denials', blank=True)
     unknown_denials = models.ManyToManyField(User, related_name='ticket_M2M_unknown_denials', blank=True)
-    adressed_users = models.ManyToManyField(User, related_name='ticket_M2M_adressed_users') #TODO: mitone khali bashe?
+    addressed_users = models.ManyToManyField(User, related_name='ticket_M2M_addressed_users') #TODO: mitone khali bashe?
     cc_users = models.ManyToManyField(User, related_name='ticket_M2M_cc_users', blank=True)
 
     contributers = models.ManyToManyField(User, related_name='ticket_M2M_contributers')
@@ -40,7 +52,7 @@ class Ticket (models.Model):
     is_public = models.BooleanField(default=True)
     being_unknown = models.BooleanField(default=False)
     tag_list = models.ManyToManyField('Tag', blank=True)
-    creation_time = models.DateField(auto_now_add=True)
+    creation_time = models.DateField(auto_now_add=True) #TODO: ezafe kardane saAt
 
     PENDING = 'PENDING'
     PROGRESSING = 'PROGRESSING'
@@ -60,9 +72,31 @@ class Ticket (models.Model):
 
     parent = models.ForeignKey('Ticket', null = True, blank = True)
 
+    @property
+    def get_contributers(self):
+        if self.being_unknown:
+            return []
+        else:
+            return self.contributers.all()
+
+    @property
+    def get_summary_body(self):
+        return self.body[0:self.summary_len]
+
+    @property
+    def get_approvers_count(self):
+        return self.known_approvers.count() + self.unknown_approvers.count()
+
+    @property
+    def get_denials_count(self):
+        return self.known_denials.count() + self.unknown_denials.count()
+
+    def __str__(self):
+        return self.title
+
 class PrivateTicket (models.Model):
     body = models.TextField()
-    adressed_users = models.ManyToManyField(User)
+    addressed_users = models.ManyToManyField(User)
     parent_ticket = models.ForeignKey('Ticket')
 
 class Type (models.Model):
@@ -75,18 +109,24 @@ class Type (models.Model):
 class Tag (models.Model):
     title = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return self.title
+
 class Department (models.Model):
     title = models.CharField(max_length=100)
     parent = models.ForeignKey('Department')
     level = models.IntegerField()
 
-class Attachments (models.Model):
+    def __str__(self):
+        return self.title
+
+class BaseAttachments (models.Model):
     path = models.CharField(max_length=500)
 
-class PublicAttachments (Attachments):
+class PublicAttachments (BaseAttachments):
     ticket = models.ForeignKey('Ticket')
 
-class PrivateAttachments (Attachments):
+class PrivateAttachments (BaseAttachments):
     ticket = models.ForeignKey('PrivateTicket')
 
 class Comments (models.Model):
--- ticket_service/serializer.py
+++ ticket_service/serializer.py
@@ -1,42 +1,65 @@
 from rest_framework import serializers
-from models import Ticket
+from models import Ticket, Type, Tag
+from django.contrib.auth.models import User
 import datetime
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'title', )
+
+class TypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = ('id', 'title')
+
+class UserSerializer(serializers.ModelSerializer):
+    picture_path = serializers.ReadOnlyField(source='profile.picture_path')
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'picture_path')
+
 class TicketSerializer(serializers.ModelSerializer):
+    contributers = UserSerializer(many=True, read_only=True, source='get_contributers')
+    # ticket_type = TypeSerializer() #TODO: baraye write bayad avaz beshe
+    summary = serializers.ReadOnlyField(source='get_summary_body')
+    approvers_count = serializers.ReadOnlyField(source='get_approvers_count')
+    denials_count = serializers.ReadOnlyField(source='get_denials_count')
+    addressed_users = UserSerializer(many=True) #TODO: test konim ke in kar mikone asan ya na!
+    cc_users = UserSerializer(many=True) #TODO: test konim ke in kar mikone asan ya na!
+    tag_list = TagSerializer(many=True)
 
     class Meta:
         model = Ticket
         fields = (
             'id',
             'title',
-            'body',
-            'summary_len',
+            'body', # write_only
+            'summary_len', # write_only
+            'in_list_contributers', # write_only
+            'summary',
             'ticket_type',
             'priority',
-
-            'known_approvers',
-#            'unknown_approvers',
-            'known_denials',
-#            'unknown_denials',
-            'adressed_users',
+            'approvers_count',
+            'denials_count',
+            'addressed_users',
             'cc_users',
-
-            'in_list_contributers',
-            'contributers',
-
             'is_public',
             'being_unknown',
             'tag_list',
-            'creation_time',
-            'status',
-
-            'need_to_confirmed',
-            'minimum_approvers_count',
-
+            'creation_time', # read_only
+            'status', # read_only
+            'contributers',
+            'need_to_confirmed', # read_only
+            'minimum_approvers_count', # read_only
             'parent',
         )
-        read_only_fields = ('contributers', 'known_approvers', 'known_denials', 'creation_time', 'status', 'need_to_confirmed', 'minimum_approvers_count')
-        extra_kwargs = {'password': {'write_only': True}}
+        read_only_fields = ('creation_time', 'status', 'need_to_confirmed', 'minimum_approvers_count')
+        extra_kwargs = {
+            'body': {'write_only': True},
+            'summary_len': {'write_only': True},
+            'in_list_contributers': {'write_only': True}
+        }
 
     def create(self, validated_data):
         ticket = Ticket(
@@ -45,22 +68,24 @@ class TicketSerializer(serializers.ModelSerializer):
             summary_len = validated_data['summary_len'],
             ticket_type = validated_data['ticket_type'],
             priority = validated_data['priority'],
-
-            known_approvers = self.context['request'].user if validated_data['being_unknown'] else None,
-            unknown_approvers = None if validated_data['being_unknown'] else self.context['request'].user,
-            contributers = self.context['request'].user,
             being_unknown = validated_data['being_unknown'],
-
             creation_time = datetime.datetime.now(),
             status = Ticket.PENDING,
-
-            adressed_users = validated_data['adressed_users'],
-            cc_users = validated_data['cc_users'],
-            in_list_contributers = validated_data['in_list_contributers'],
-
             is_public = validated_data['is_public'],
-            tag_list = validated_data['tag_list'],
             parent = validated_data['parent'],
         )
         ticket.save()
+
+        # TODO: khode user ro az list 'in_list_contributers' o 'addressed_users' o 'cc_users' ina hazf konim!
+        ticket.contributers.add(self.context['request'].user)
+        map(lambda user: ticket.addressed_users.add(user), validated_data['addressed_users'])
+        map(lambda user: ticket.cc_users.add(user), validated_data['cc_users'])
+        map(lambda user: ticket.in_list_contributers.add(user), validated_data['in_list_contributers'])
+        map(lambda tag: ticket.tag_list.add(tag), validated_data['tag_list'])
+
+        if not validated_data['being_unknown']:
+            ticket.known_approvers.add(self.context['request'].user)
+        else:
+            ticket.unknown_approvers.add(self.context['request'].user)
+
         return ticket
