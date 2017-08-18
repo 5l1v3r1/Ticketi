744cda16cb25702f06cefe537b506e0839fb9f6c
SadeghHayeri <hayerisadegh@gmail.com>
2017-08-18 15:58:47 +0430

    IsInListContributers and LikeOwner added.

--- ticket_service/permissions.py
+++ ticket_service/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+from .models import Ticket
 
-class IsOwnerOrReadOnly(BasePermission):
+class IsOwnerOrReadOnly(BasePermission): #TODO: edit
     my_safe_method = ['GET', 'PUT']
     def has_permission(self, request, view):
         if request.method in self.my_safe_method:
@@ -9,3 +10,49 @@ class IsOwnerOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, ticket):
         return request.user in ticket.contributers.all()
+
+class IsInListContributers(BasePermission):
+    my_safe_method = ['GET', 'PATCH'] #TODO: remove 'GET' for production
+    def has_permission(self, request, view):
+
+        # check request method
+        if not request.method in self.my_safe_method:
+            return False
+
+        # check object permission
+        ticket_id = view.kwargs.get('pk', None)
+
+        if not ticket_id:
+            return False
+
+        ticket = Ticket.objects.filter(id=ticket_id).first()
+        if not ticket:
+            return False
+
+        if request.user in ticket.in_list_contributers.all():
+            return True
+
+        return False
+
+class LikeOwner(BasePermission):
+    my_safe_method = ['DELETE']
+    def has_permission(self, request, view):
+
+        # check request method
+        if not request.method in self.my_safe_method:
+            return False
+
+        # check object permission
+        ticket_id = view.kwargs.get('pk', None)
+
+        if not ticket_id:
+            return False
+
+        ticket = Ticket.objects.filter(id=ticket_id).first()
+        if not ticket:
+            return False
+
+        if request.user in ticket.in_list_contributers.all():
+            return True
+
+        return False
